6e4a1deb51ecdcac1b57edd737f1085a11a3efa0
semyon-ter <semen010191@gmail.com>
2020-09-09 12:44:49 +0300

    исправление изменений в Оплате, перенос правок от shurinskiy в верстку

--- html/blocks/head.php
+++ html/blocks/head.php
@@ -1,30 +1,33 @@
 
 <header data-top_offset="50" data-bg_color="#5922e1" class="navi navi_top">
-	<div class="navi__container"><span class="menu-open"></span>
-		<ul class="navi__menu menu"><span class="menu-close"></span>
-			<li class="menu-item"><a href="./">Главная</a></li>
-			<li class="menu-item menu-item-has-children"><a href="./">Принципы работы</a>
-				<ul class="sub-menu">
-					<li class="menu-item"><a href="./">Consectetur</a></li>
-					<li class="menu-item"><a href="./">Adipisicing</a></li>
-					<li class="menu-item"><a href="./">Reprehenderit</a></li>
-					<li class="menu-item"><a href="./">Ipsum dolor</a></li>
+	<div class="navi__container"><span class="menu__open"></span>
+		<ul class="navi__menu menu"><span class="menu__close"></span>
+			<li class="menu__item"><a href="./">Главная</a></li>
+			<li class="menu__item menu__item_haschild"><a href="./">Принципы работы</a>
+				<ul class="menu_submenu">
+					<li class="menu__item"><a href="./">Consectetur</a></li>
+					<li class="menu__item"><a href="./">Adipisicing</a></li>
+					<li class="menu__item"><a href="./">Reprehenderit</a></li>
+					<li class="menu__item"><a href="./">Ipsum dolor</a></li>
 				</ul>
 			</li>
-			<li class="menu-item menu-item-has-children"><a href="./">Чекапы</a>
-				<ul class="sub-menu">
-					<li class="menu-item"><a href="./">Базовый</a></li>
-					<li class="menu-item"><a href="./">Полный</a></li>
-					<li class="menu-item"><a href="./">Метабокардио</a></li>
-					<li class="menu-item"><a href="./">Сон</a></li>
-					<li class="menu-item"><a href="./">Витамин D</a></li>
+			<li class="menu__item menu__item_haschild"><a href="./">Чекапы</a>
+				<ul class="menu_submenu">
+					<li class="menu__item"><a href="./">Базовый</a></li>
+					<li class="menu__item"><a href="./">Полный</a></li>
+					<li class="menu__item"><a href="./">Метабокардио</a></li>
+					<li class="menu__item"><a href="./">Сон</a></li>
+					<li class="menu__item"><a href="./">Витамин D</a></li>
 				</ul>
 			</li>
-			<li class="menu-item"><a href="./">Приложение</a></li>
-			<li class="menu-item"><a href="./">FAQ</a></li>
-			<li class="menu-item"><a href="./">О нас</a></li>
-			<li class="menu-item"><a href="./">Контакты</a></li>
+			<li class="menu__item"><a href="./">Приложение</a></li>
+			<li class="menu__item"><a href="./">FAQ</a></li>
+			<li class="menu__item"><a href="./">О нас</a></li>
+			<li class="menu__item"><a href="./">Контакты</a></li>
 		</ul>
-		<div class="navi__lang"><a href="./" class="navi__lang-active">RU</a><a href="./">EN</a></div><a href="./" class="navi__login"></a><a href="./" class="navi__cart"><span class="navi__cart-counter navi__cart-counter_noempty">99</span></a>
+		<div class="navi__lang"><a href="./" class="navi__lang-active">RU</a><a href="./">EN</a></div>
+		<div class="navi__user user">
+			<div class="user__wrapper"><a href="./" class="user__link">Профиль</a><a href="./" class="user__link">История покупок</a></div>
+		</div><a href="./" class="navi__cart"><span class="navi__cart-counter navi__cart-counter_noempty">99</span></a>
 	</div>
 </header>
--- html/footer.php
+++ html/footer.php
@@ -18,28 +18,28 @@
 	<div class="footer__navi navi navi_footer">
 		<div class="navi__container">
 			<ul class="navi__menu menu">
-				<li class="menu-item"><a href="./">Главная</a></li>
-				<li class="menu-item menu-item-has-children"><a href="./">Принципы работы</a>
-					<ul class="sub-menu">
-						<li class="menu-item"><a href="./">Consectetur</a></li>
-						<li class="menu-item"><a href="./">Adipisicing</a></li>
-						<li class="menu-item"><a href="./">Reprehenderit</a></li>
-						<li class="menu-item"><a href="./">Ipsum dolor</a></li>
+				<li class="menu__item"><a href="./">Главная</a></li>
+				<li class="menu__item menu__item_haschild"><a href="./">Принципы работы</a>
+					<ul class="menu_submenu">
+						<li class="menu__item"><a href="./">Consectetur</a></li>
+						<li class="menu__item"><a href="./">Adipisicing</a></li>
+						<li class="menu__item"><a href="./">Reprehenderit</a></li>
+						<li class="menu__item"><a href="./">Ipsum dolor</a></li>
 					</ul>
 				</li>
-				<li class="menu-item menu-item-has-children"><a href="./">Чекапы</a>
-					<ul class="sub-menu">
-						<li class="menu-item"><a href="./">Базовый</a></li>
-						<li class="menu-item"><a href="./">Полный</a></li>
-						<li class="menu-item"><a href="./">Метабокардио</a></li>
-						<li class="menu-item"><a href="./">Сон</a></li>
-						<li class="menu-item"><a href="./">Витамин D</a></li>
+				<li class="menu__item menu__item_haschild"><a href="./">Чекапы</a>
+					<ul class="menu_submenu">
+						<li class="menu__item"><a href="./">Базовый</a></li>
+						<li class="menu__item"><a href="./">Полный</a></li>
+						<li class="menu__item"><a href="./">Метабокардио</a></li>
+						<li class="menu__item"><a href="./">Сон</a></li>
+						<li class="menu__item"><a href="./">Витамин D</a></li>
 					</ul>
 				</li>
-				<li class="menu-item"><a href="./">Приложение</a></li>
-				<li class="menu-item"><a href="./">FAQ</a></li>
-				<li class="menu-item"><a href="./">О нас</a></li>
-				<li class="menu-item"><a href="./">Контакты</a></li>
+				<li class="menu__item"><a href="./">Приложение</a></li>
+				<li class="menu__item"><a href="./">FAQ</a></li>
+				<li class="menu__item"><a href="./">О нас</a></li>
+				<li class="menu__item"><a href="./">Контакты</a></li>
 			</ul>
 			<div class="navi__lang"><a href="./" class="navi__lang-active">RU</a><a href="./">EN</a></div>
 		</div>
