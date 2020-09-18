6561028e2212405f5ea67c9b99cebb8143219812
semyon-ter <semen010191@gmail.com>
2020-09-18 14:03:05 +0300

    перенос изменений от shurinskiy, корректировка собственной верстки как следствие первого

--- html/page/plus-product.php
+++ html/page/plus-product.php
@@ -4,31 +4,31 @@
 		<h2 class="section__title title title_color-1">5 основных<span>преимуществ</span></h2>
 	</div>
 	<div class="section__content section__content_wide v2 how__items border-decor border-decor__child border-decor__color-3 border-decor__tac">
-		<div class="how__item">
+		<div class="how__item col-20 col-desk-25 col-pad-1-3 col-mob-100">
 			<div class="how__item-number t t-color3 mb-20">1</div>
 			<div class="how__item-text t t-color4">
 				<p>Высокоспецифичный и чувствительный анализ сухой крови имеет высокую корреляцию с обычными сывороточными анализами для профилактики ССЗ.</p>
 			</div>
 		</div>
-		<div class="how__item">
+		<div class="how__item col-20 col-desk-25 col-pad-1-3 col-mob-100">
 			<div class="how__item-number t t-color3 mb-20">2</div>
 			<div class="how__item-text t t-color4">
 				<p>Интуитивно простой и безболезненный тест, позволяющий быстро собрать несколько капель крови из пальца на специальную фильтровальную бумагу из набора.</p>
 			</div>
 		</div>
-		<div class="how__item">
+		<div class="how__item col-20 col-desk-25 col-pad-1-3 col-mob-100">
 			<div class="how__item-number t t-color3 mb-20">3</div>
 			<div class="how__item-text t t-color4">
 				<p>Образцы сухой крови стабильны в течение нескольких недель и имеют очень небольшой риск инфекционных заболеваний, таких как ВИЧ, так как они инактивируются в сухом виде.</p>
 			</div>
 		</div>
-		<div class="how__item">
+		<div class="how__item col-20 col-desk-25 col-pad-1-3 col-mob-100">
 			<div class="how__item-number t t-color3 mb-20">4</div>
 			<div class="how__item-text t t-color4">
 				<p>Не требуется никаких лабораторных или дополнительных расходов на доставку.</p>
 			</div>
 		</div>
-		<div class="how__item">
+		<div class="how__item col-20 col-desk-25 col-pad-1-3 col-mob-100">
 			<div class="how__item-number t t-color3 mb-20">5</div>
 			<div class="how__item-text t t-color4">
 				<p>Опционально, на основе комплексного теста Метабокардио чекап можно будет получить консультации с известными медицинскими экспертами и создать индивидуальную программу лечения.</p>
--- html/page/plus.php
+++ html/page/plus.php
@@ -4,31 +4,31 @@
 		<h2 class="section__title title title_color-1">Преимущества приложения<span>diagnost</span></h2>
 	</div>
 	<div class="section__content section__content_wide v2 how__items border-decor border-decor__child border-decor__color-3 border-decor__tac">
-		<div class="how__item">
+		<div class="how__item col-20 col-desk-25 col-pad-1-3 col-mob-100">
 			<div class="how__item-number t t-color3 mb-20">1</div>
 			<div class="how__item-text t t-color4">
 				<p>Современное и легкое в понимании приложение ДИАГНОСТ поможет вам интуитивно выбрать правильный тест на основе ваших симптомов и личных параметров вашего здоровья</p>
 			</div>
 		</div>
-		<div class="how__item">
+		<div class="how__item col-20 col-desk-25 col-pad-1-3 col-mob-100">
 			<div class="how__item-number t t-color3 mb-20">2</div>
 			<div class="how__item-text t t-color4">
 				<p>Наше приложение упрощает доступ к вашему личному трекеру здоровья на платформе диагностики в любом месте</p>
 			</div>
 		</div>
-		<div class="how__item">
+		<div class="how__item col-20 col-desk-25 col-pad-1-3 col-mob-100">
 			<div class="how__item-number t t-color3 mb-20">3</div>
 			<div class="how__item-text t t-color4">
 				<p>У вас будет доступ к БЕСПЛАТНОЙ проверке состояния здоровья БАЗОВЫЙ ЧЕКАП с подробным объяснением ваших параметров в приложении</p>
 			</div>
 		</div>
-		<div class="how__item">
+		<div class="how__item col-20 col-desk-25 col-pad-1-3 col-mob-100">
 			<div class="how__item-number t t-color3 mb-20">4</div>
 			<div class="how__item-text t t-color4">
 				<p>Легко выбрать, заказать и приобрести любой тест; наши тесты просты и не инвазивны, с четкими инструкциями</p>
 			</div>
 		</div>
-		<div class="how__item">
+		<div class="how__item col-20 col-desk-25 col-pad-1-3 col-mob-100">
 			<div class="how__item-number t t-color3 mb-20">5</div>
 			<div class="how__item-text t t-color4">
 				<p>Каждый тест анализируется и интерпретируется командой экспертов в области превентивной медицины; подробный визуальный отчет доступен в приложении</p>
